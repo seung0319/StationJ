824dd8cca81152e48866922f1d964301c1247750
seung0319 <nnn0319@naver.com>
2024-02-14 15:29:05 +0900

    chore-지도화면 카테고리 버튼 편집

--- Assets/02. Scripts/TabManager.cs
+++ Assets/02. Scripts/TabManager.cs
@@ -6,13 +6,20 @@ using UnityEngine.UI;
 public class TabManager : MonoBehaviour
 {
     public GameObject[] panels; // 이 배열에 각각의 패널을 할당합니다.
+
+    // 카테고리 버튼들에 대한 참조
+    public Button[] categoryButtons;
+
+    // 현재 선택된 카테고리 버튼에 대한 참조
+    private Button selectedCategoryButton;
+
     public Text[] texts;
     private Color selectedColor = new Color32(54, 148, 215, 255); // 선택된 버튼의 색
     private Color unselectedColor = new Color32(185, 189, 189, 255); // 선택되지 않은 버튼의 색
 
     private void Start()
     {
-        texts[3].GetComponentInParent<Button>().Select();
+        
     }
 
     public void ShowPanel(int panelIndex) // 이 함수는 각 탭의 OnClick 이벤트에 연결합니다.
@@ -40,5 +47,20 @@ public class TabManager : MonoBehaviour
             texts[i].color = unselectedColor;
         }
         texts[3].color = selectedColor;
+    }
+
+    public void OnCategoryButtonClicked()
+    {
+        Button clickedButton = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.GetComponent<Button>();
+        // 이전에 선택된 버튼의 상태를 '선택 안 함'으로 변경
+        if (selectedCategoryButton != null)
+        {
+            // 버튼의 Sprite를 Normal Sprite로 변경합니다.
+            selectedCategoryButton.image.sprite = selectedCategoryButton.spriteState.disabledSprite;
+        }
+
+        // 클릭한 버튼의 Sprite를 Pressed Sprite로 변경
+        clickedButton.image.sprite = clickedButton.spriteState.pressedSprite;
+        selectedCategoryButton = clickedButton;
     }
 }
