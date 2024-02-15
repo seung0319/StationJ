599d6e2e9b9eea367fb20e95adb0f91b57d51272
pkj0122 <gkzlxm060@naver.com>
2024-02-15 15:24:42 +0900

    feat - 위치정보접근 허용팝업 구현

--- Assets/02. Scripts/TabManager.cs
+++ Assets/02. Scripts/TabManager.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Android;
 using UnityEngine.UI;
 
 public class TabManager : MonoBehaviour
 {
+    public GameObject locationPanel;
+    public GameObject makerPanel;
+    public GameObject routeFindPanerl;
+    private int order = 0;
+
+    private bool locationOK;
+
     public GameObject[] panels; // 이 배열에 각각의 패널을 할당합니다.
 
     // 카테고리 버튼들에 대한 참조
@@ -19,7 +27,13 @@ public class TabManager : MonoBehaviour
 
     private void Start()
     {
-        
+        locationOK = Permission.HasUserAuthorizedPermission(Permission.FineLocation);
+
+        if (!locationOK)
+        {
+            locationPanel.SetActive(true);
+            makerPanel.SetActive(false);
+        }
     }
 
     public void ShowPanel(int panelIndex) // 이 함수는 각 탭의 OnClick 이벤트에 연결합니다.
@@ -62,5 +76,46 @@ public class TabManager : MonoBehaviour
         // 클릭한 버튼의 Sprite를 Pressed Sprite로 변경
         clickedButton.image.sprite = clickedButton.spriteState.pressedSprite;
         selectedCategoryButton = clickedButton;
+    }
+
+    public void LocationInfoAllow(bool Allow)
+    {
+        if(order == 0)  //첫번째 팝업창의 경우
+        {
+            if (Allow)  //위치권한요청 허용
+            {
+                UnityEngine.Android.Permission.RequestUserPermission(UnityEngine.Android.Permission.FineLocation);
+                locationOK = true;
+                order++;
+            }
+            else        //위치권한요청 허용 안함
+            {
+                order++;
+            }
+        }
+        else if(order == 1)  //두번째 팝업창의 경우
+        {
+            if (Allow)  //위치권한요청 허용
+            {
+                UnityEngine.Android.Permission.RequestUserPermission(UnityEngine.Android.Permission.FineLocation);
+                locationOK = true;
+                routeFindPanerl.SetActive(true);
+            }
+            else        //위치권한요청 허용 안함
+            {
+                
+            }
+        }
+    }
+    public void RouteFind()
+    {
+        if (!locationOK)
+        {
+            locationPanel.SetActive(true);
+        }
+        else
+        {
+            routeFindPanerl.SetActive(true);
+        }
     }
 }
