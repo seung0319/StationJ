28fa72b9cac7472dde6da81d01970d9c82c976c2
seung0319 <nnn0319@naver.com>
2024-02-20 16:34:13 +0900

    feat: 데이터로드를 위한 POI 데이터클래스 추가

---
+++ Assets/02. Scripts/POI.cs
@@ -0,0 +1,24 @@
+/// <summary>
+/// Json을 저장할 클래스
+/// 이름, 건물종류, 건물사진, 위도, 경도, 주소, 설명 를 가지고 있습니다.
+/// </summary>
+[System.Serializable]
+public class POI
+{
+    public string name;
+    public string type;
+    public string image;
+    public float latitude;
+    public float longitude;
+    public string address;
+    public string description;
+}
+
+/// <summary>
+/// 위 항목들의 배열을 저장하는 클래스
+/// </summary>
+[System.Serializable]
+public class POIList
+{
+    public POI[] pois;
+}
