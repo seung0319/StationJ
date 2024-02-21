cc1ea534577bc1c2d30d417c4e7cfa11ce2a72c5
iven612 <andako6@gmail.com>
2024-02-21 17:08:31 +0900

    feat: 도슨트
    도슨트 이미지 인식시 3d오브젝트 생성 인식범위를 벗어나면 다시 생성

--- Assets/02. Scripts/PlayTimeLine.cs
+++ Assets/02. Scripts/PlayTimeLine.cs
@@ -1,14 +1,12 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Video;
+
 
 public class PlayTimeLine : MonoBehaviour
 {
     /// 터치 코드 ///
-    public VideoPlayer videoPlayer;
-    //public Slider timelineSlider;
+    /*public VideoPlayer videoPlayer;
     public Button replayButton;
     private bool isPaused = true;
     private bool isTouching = false;
@@ -17,9 +15,6 @@ public class PlayTimeLine : MonoBehaviour
     {
         videoPlayer.loopPointReached += OnVideoEnd;
         replayButton.onClick.AddListener(ReplayVideo);
-
-        //timelineSlider.minValue = 0f;
-       // timelineSlider.maxValue = (float)videoPlayer.clip.length;
     }
 
     void OnVideoEnd(VideoPlayer vp)
@@ -69,15 +64,8 @@ public class PlayTimeLine : MonoBehaviour
         {
            // timelineSlider.value = (float)videoPlayer.time;
         }
-    }
-
-    public void OnSliderValueChanged()
-    {
-        if (!isPaused)
-        {
-            //videoPlayer.time =// timelineSlider.value;
-        }
-    }
+    }*/
+    ///터치코드 끝
 
     //public VideoPlayer videoPlayer;
     //public Slider timelineSlider;
@@ -136,4 +124,138 @@ public class PlayTimeLine : MonoBehaviour
     //        videoPlayer.time = timelineSlider.value;
     //    }
     //}
+
+    public List<AudioSource> audioSources;
+    public Slider timelineSlider;
+    public Button replayButton;
+    private bool isPaused = true;
+    private bool isTouching = false;
+
+    void Start()
+    {
+        foreach (AudioSource audioSource in audioSources)
+        {
+            audioSource.loop = false;
+            audioSource.playOnAwake = false;
+        }
+
+        replayButton.onClick.AddListener(ReplayAudio);
+        timelineSlider.minValue = 0f;
+        timelineSlider.maxValue = GetLongestClipLength();
+    }
+
+    void ReplayAudio()
+    {
+        foreach (AudioSource audioSource in audioSources)
+        {
+            audioSource.Stop();
+            audioSource.Play();
+            isPaused = false;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                isTouching = true;
+            }
+        }
+        else
+        {
+            isTouching = false;
+        }
+
+        if (isTouching)
+        {
+            if (IsAnyAudioPlaying() && !isPaused)
+            {
+                PauseAllAudio();
+                isPaused = true;
+            }
+            else if (isPaused)
+            {
+                PlayAllAudio();
+                isPaused = false;
+            }
+        }
+
+        if (!isPaused)
+        {
+            timelineSlider.value = GetMaxAudioTime();
+        }
+    }
+
+    public void OnSliderValueChanged()
+    {
+        if (!isPaused)
+        {
+            SetAllAudioTime(timelineSlider.value);
+        }
+    }
+
+    private float GetLongestClipLength()
+    {
+        float maxLength = 0f;
+        foreach (AudioSource audioSource in audioSources)
+        {
+            if (audioSource.clip != null && audioSource.clip.length > maxLength)
+            {
+                maxLength = audioSource.clip.length;
+            }
+        }
+        return maxLength;
+    }
+
+    private bool IsAnyAudioPlaying()
+    {
+        foreach (AudioSource audioSource in audioSources)
+        {
+            if (audioSource.isPlaying)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void PauseAllAudio()
+    {
+        foreach (AudioSource audioSource in audioSources)
+        {
+            audioSource.Pause();
+        }
+    }
+
+    private void PlayAllAudio()
+    {
+        foreach (AudioSource audioSource in audioSources)
+        {
+            audioSource.Play();
+        }
+    }
+
+    private float GetMaxAudioTime()
+    {
+        float maxTime = 0f;
+        foreach (AudioSource audioSource in audioSources)
+        {
+            if (audioSource.isPlaying && audioSource.time > maxTime)
+            {
+                maxTime = audioSource.time;
+            }
+        }
+        return maxTime;
+    }
+
+    private void SetAllAudioTime(float time)
+    {
+        foreach (AudioSource audioSource in audioSources)
+        {
+            audioSource.time = time;
+        }
+    }
 }
